73164afa0f79df6b4d774c082dc4d683a6dc5f8c
Vaibhav Kumar Thakur <vaibhavkr522@gmail.com>
2024-03-30 00:35:56 +0530

    SignApi

--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
 
 dotenv.config();
 mongoose
@@ -12,6 +14,10 @@ mongoose
     console.log(err);
   });
 const app = express();
+app.use(express.json());
 app.listen(3000, () => {
   console.log("Server is running on port 3000!");
 });
+
+app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
